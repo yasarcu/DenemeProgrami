5f77ab76515bf7f936b4565498cf5afabf2245bc
yasarcu <yasarcu@hotmail.com>
2017-10-09 18:49:06 +0200

    New branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Pull request
             Console.WriteLine("Meraba Mete");
             Console.Read();
         }
